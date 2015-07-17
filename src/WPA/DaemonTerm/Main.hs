b6eeb5d158818c896e6d73eab683ee94ad230e69
kahless <krantadan@mail.ru>
2015-07-17 14:36:34 +0300

    wpa_daemon без псевдотерминала

--- src/WPA/DaemonTerm/Main.hs
+++ src/WPA/DaemonTerm/Main.hs
@@ -10,13 +10,15 @@ rg daemon.hs -i wlp3s0 -D nl80211 -c /etc/netm/nest/_wlan/wpa_supplicant.conf
 -}
 
 
-import Control.Monad (forever)
+import Control.Monad (forever, forM)
 import System.Environment (getArgs)
 import System.Process
 import Control.Concurrent
 import System.IO
 import System.Exit
+import System.Posix.Terminal
 import System.Posix.IO
+import Data.List.Split (endBy)
 
 import System.Log
 import System.Log.Handler
@@ -31,25 +33,30 @@ main = do
 
    logH <- openlog "wpa_supplicant" [] DAEMON NOTICE
 
+   -- Создание псевдотерминала, через который будет проходить общение с 
+   -- wpa_supplicant. Если запустить его не через терминал, то он станет очень 
+   -- молчалив. Ключами вернуть дефолтный уровень словесности невозможно.
+   (master, slave) <- openPseudoTerminal
+   hslave <- fdToHandle slave
+   hSetBuffering hslave NoBuffering
+
    -- Запуск wpa_supplicant
    args <- getArgs
-   (_, Just out, Just err, p) <- createProcess (proc "wpa_supplicant" args)
-      { std_out = CreatePipe
-      , std_err = CreatePipe
+   (_, _, _, p) <- createProcess (proc "wpa_supplicant" args)
+      { env = Just [ ("TERM", "xterm") ]
+      , std_in  = UseHandle hslave
+      , std_out = UseHandle hslave
+      , std_err = UseHandle hslave
       , close_fds = True
       , delegate_ctlc = True
       }
 
-   -- stdout
+   -- Читаю вывод wpa_supplicant из созданного псевдотерминала
    forkIO $ forever $ do
-      l <- hGetLine out
-      putStrLn l >> hFlush stdout
-      logStr logH l
-
-   -- stderr
-   forkIO $ forever $ do
-      l <- hGetLine err
-      hPutStrLn stderr l >> hFlush stderr
-      logStr logH l
+      (s, _) <- fdRead master 1024
+      let ls = endBy "\n" . filter (/='\r') $ s
+      forM ls $ \ l -> do
+         putStrLn l >> hFlush stdout
+         logStr logH l
 
    exitWith =<< waitForProcess p
